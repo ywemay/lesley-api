ae9ea6ca93e26cb1ef9e880685d43f1f4324e227
ywemay <srv023@ywemay.com>
2020-02-07 00:13:57 +0800

    Add product entity

---
+++ src/Entity/Product.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ApiResource()
+ * @ORM\Entity(repositoryClass="App\Repository\ProductRepository")
+ */
+class Product
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=160)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $sell_price;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getSellPrice(): ?int
+    {
+        return $this->sell_price;
+    }
+
+    public function setSellPrice(int $sell_price): self
+    {
+        $this->sell_price = $sell_price;
+
+        return $this;
+    }
+}
